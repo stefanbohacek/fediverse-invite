99c1ba4ae114f32ae653b4cd8c88d2a6e31adfeb
Stefan Bohacek <stefan@stefanbohacek.com>
2026-05-31 17:09:15 -0400

    Updated starter pack section layout based on the number of starter packs.

--- public/js/modules/showStarterPacks.js
+++ public/js/modules/showStarterPacks.js
@@ -7,12 +7,22 @@ export default async () => {
     const starterPackData = await resp.json();
 
     if (starterPackData?.length) {
+      const starterPackCount = starterPackData.length;
       const starterPacksResults = document.getElementById("starter-packs");
       let starterPackHTML = "";
+      let colClass;
+
+      if (starterPackCount <= 2) {
+        colClass = "col-12 col-sm-6";
+      } else if (starterPackCount === 3) {
+        colClass = "col-12 col-sm-6 col-md-4";
+      } else {
+        colClass = "col-12 col-sm-6 col-md-4 col-lg-3";
+      }
 
       starterPackData.forEach((starterPack) => {
         starterPackHTML += /* html */ `
-          <div class="col-12 col-sm-6 col-md-4 col-lg-3 pb-4">
+          <div class="${colClass} pb-4">
             <div class="card h-100">
               <div class="card-body">
                 <span class="badge rounded-pill text-bg-info float-end fs-7">${starterPack.accounts.length.toLocaleString()} ${window.translations.accounts}</span>
